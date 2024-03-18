9b08c7b923bd246940b6b4f4a796d9c991f652aa
Tatiana Shvetsova <Shvetsovat82@gmail.com>
2024-03-18 16:28:44 +0400

    Lesson 3: add Task2

---
+++ Lesson3/Les3_Task2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Вывод на экран квадратов чисел от 1 до N. 
+
+void PrintSquears(int limit)
+{
+    int i = 1;
+    while (i <= limit)
+    {
+        Console.Write($"{i * i} ");
+        i++;
+    }
+}
+
+PrintSquears(5);
+PrintSquears(10);
+PrintSquears(15);
