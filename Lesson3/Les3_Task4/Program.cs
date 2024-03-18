804309c457abc7e54d025e70bd36c3f7a1106451
Tatiana Shvetsova <Shvetsovat82@gmail.com>
2024-03-18 19:36:58 +0400

    Lesson 3: add Task4

---
+++ Lesson3/Les3_Task4/Program.cs
@@ -0,0 +1,56 @@
+﻿int n = 10;
+int[] arr = new int[n];
+
+void FillArray(int[] arr)
+{
+    int i = 0;
+    while (i < n)
+    {
+        arr[i] = i + 1;
+        i++;
+    }
+}
+FillArray(arr);
+
+void PrintArray(int[] arr)
+{
+    int i = 0;
+    while (i < arr.Length)
+    {
+        Console.Write($"{arr[i]} ");
+        i++;
+    }
+}
+
+PrintArray(arr);
+
+int GetSumOfElements(int[] arr)
+{
+    int i = 0;
+    int sum = 0;
+    while (i < arr.Length)
+    {
+        sum = sum + arr[i];
+        i++;
+    }
+    return sum;
+}
+int sum = GetSumOfElements(arr);
+
+Console.WriteLine(sum);
+
+int GetProductOfElements(int[] arr)
+{
+    int i = 0;
+    int product = 1;
+    while (i < arr.Length)
+    {
+        product = product * arr[i];
+        i++;
+    }
+    return product;
+}
+
+int product = GetProductOfElements(arr);
+
+Console.WriteLine(product);
