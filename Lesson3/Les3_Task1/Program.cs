59fa3ae4a52195db854f349e8ba9fe843a1ad683
Tatiana Shvetsova <Shvetsovat82@gmail.com>
2024-03-18 15:52:39 +0400

    Lesson 3: add Task1

---
+++ Lesson3/Les3_Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Вычислить значение формулы (a x b)/(c+d), где a, b, c, d - некоторые числа.
+// Решение оформить в виде функции.
+
+double CalculateFormula(int a, int b, int c, int d)
+{
+    double numenator = a * b;
+    int denumenator = c + d;
+    double result = numenator / denumenator;
+    return result;
+}
+
+double result = CalculateFormula(1, 2, 3, 4);
+
+Console.WriteLine(result);
