b80e8884982e11faa240d2d8bedb2eedbb64338d
Tatiana Shvetsova <Shvetsovat82@gmail.com>
2024-03-24 00:04:49 +0400

    changin Lesson4: Task1

--- Lesson4/Les4_Task1/Program.cs
+++ Lesson4/Les4_Task1/Program.cs
@@ -23,7 +23,7 @@ void ShowMatrix(int[,] matrix)
     {
         for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            Console.Write($"{matrix[1, j]} ");
+            Console.Write($"{matrix[i, j]} ");
         }
         Console.WriteLine();
     }
